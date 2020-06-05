cb1589b421b217cd1a4b70d5355cc0c37f236971
Ailandas <50612411+Ailandas@users.noreply.github.com>
2020-06-05 13:51:34 +0300

    Tests

---
+++ Tests/Feature/GetWordsFromCategory.feature
@@ -0,0 +1,11 @@
+﻿Feature: GetWordsFromCategory
+	In order to avoid silly mistakes
+	As a math idiot
+	I want to be told the sum of two numbers
+
+@CategoryEntries
+Scenario: I wanna get all words from a certain category
+	Given I have a link
+	When When I enter a category
+	And Press enter
+	Then the result should be visible
---
+++ Tests/Steps/GetWordsFromCategorySteps.cs
@@ -0,0 +1,51 @@
+﻿using NUnit.Framework;
+using System;
+using System.Net.Http;
+using TechTalk.SpecFlow;
+
+namespace Tests.Steps
+{
+    [Binding]
+    public class GetWordsFromCategorySteps
+    {
+        public HttpClient client;
+        public string baseAddress;
+        public HttpResponseMessage response;
+
+        [Given(@"I have a link")]
+        public void GivenIHaveALink()
+        {
+            client = new HttpClient();
+        }
+        
+        [When(@"When I enter a category")]
+        public void WhenWhenIEnterACategory()
+        {
+            baseAddress = "http://localhost:58065/api/quotedictionary/categories/" + "Crime";
+        }
+        
+        [When(@"Press enter")]
+        public void WhenPressEnter()
+        {
+             response = client.GetAsync(baseAddress).Result;
+        }
+        
+        [Then(@"the result should be visible")]
+        public async void ThenTheResultShouldBeOnTheScreen()
+        {
+            if (response.IsSuccessStatusCode)
+            {
+                Console.WriteLine("Request Message Information:- \n\n" + response.RequestMessage + "\n");
+                Console.WriteLine("Response Message Header \n\n" + response.Content.Headers + "\n");
+                // Get the response
+                var customerJsonString = await response.Content.ReadAsStringAsync();
+                Console.WriteLine("Your response data is: " + customerJsonString);
+            }
+            else
+            {
+                throw new Exception("Error");
+            }
+            
+        }
+    }
+}
